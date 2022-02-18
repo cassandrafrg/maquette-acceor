6607bde4e035e39ba38fe7880e9f297d304e5540
cassandrafrg <cassandraf@live.fr>
2022-02-18 17:00:54 +0100

    three

--- Acceor/script.js
+++ Acceor/script.js
@@ -4,4 +4,25 @@ $('.slider').slick({
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
 });
